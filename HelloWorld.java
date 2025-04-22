aa7c6aa76e6922ea651d127eb527280adff3fb46
anand5206 <ananddeepakjadhav@gmail.com>
2025-04-22 23:44:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,22 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+
+    // Constructor
+    public HelloWorld() {
+        System.out.println("Hello from the constructor!");
+    }
+
+    // Method to print Hello, World!
+    public void sayHello() {
         System.out.println("Hello, World!");
     }
+
+    // Main method
+    public static void main(String[] args) {
+        // Create an object of HelloWorld
+        HelloWorld hello = new HelloWorld();
+
+        // Call the sayHello method
+        hello.sayHello();
+    }
 }
+
